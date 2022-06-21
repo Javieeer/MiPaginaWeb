a4fc9e58da8f4e132c6bf0395967c49d042e34c4
Javieeer <zapatajavier34@gmail.com>
2022-06-20 23:02:42 -0500

    Se edita la ruta para poder visualizar en internet

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
 
-@app.route('/MiPaginaWeb/')
+@app.route('https://javieeer.github.io/MiPaginaWeb/')
 def index():
     return render_template('index.html')
 
